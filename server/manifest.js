d1ee83a5c3d2f4fafd1a49471efbff5d2a5ac8e0
Guillaume MAUREL <guillaume.maurel@etu.unilim.fr>
2019-02-27 13:58:50 +0100

    Remise au propre du git pour rendre le projet

---
+++ server/manifest.js
@@ -0,0 +1,74 @@
+'use strict';
+
+const Confidence = require('confidence');
+const Toys = require('toys');
+
+require('dotenv').config()
+
+// Glue manifest as a confidence store
+module.exports = new Confidence.Store({
+    server: {
+        host: 'localhost',
+        port: {
+            $env: 'PORT',
+            $coerce: 'number',
+            $default: 3000
+        },
+        debug: {
+            $filter: { $env: 'NODE_ENV' },
+            $default: {
+                log: ['error'],
+                request: ['error']
+            },
+            production: {
+                request: ['implementation']
+            }
+        }
+    },
+    register: {
+        plugins: [
+            {
+                plugin: '../lib', // Main plugin
+                options: {}
+            },
+            {
+                plugin: './plugins/swagger'
+            },
+            {
+
+
+                plugin: 'schwifty',
+                options: {
+                    $filter: 'NODE_ENV',
+                    $default: {},
+                    $base: {
+                        migrateOnStart: true,
+                        knex: {
+
+                            client: 'pg',
+                            useNullAsDefault: true,     // Suggested for sqlite3
+                            connection: {
+                                host: process.env.POSTGRES_HOST,
+                                port: process.env.POSTGRES_PORT,
+                                user: process.env.POSTGRES_USER,
+                                password: process.env.POSTGRES_PASSWORD,
+                                database: process.env.POSTGRES_DATABASE
+
+                            }
+                        }
+                    },
+                    production: {
+                        migrateOnStart: false
+                    }
+                }
+            },
+            {
+                plugin: {
+                    $filter: { $env: 'NODE_ENV' },
+                    $default: 'hpal-debug',
+                    production: Toys.noop
+                }
+            }
+        ]
+    }
+});
